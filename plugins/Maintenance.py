05902d5e8bf1a662a18e9faf092458c132c96d21
anujedits42-byte <anujedits42@gmail.com>
2026-04-05 02:48:52 +0530

    Update Maintenance.py

--- plugins/Maintenance.py
+++ plugins/Maintenance.py
@@ -11,7 +11,7 @@ async def maintenance_checker(client, message: Message):
     chat_id = message.chat.id
     if MAINTENANCE_MODE and user_id not in BYPASS_IDS and chat_id not in BYPASS_IDS:
         await message.reply(
-            "🚧 **Bot is under Maintenance!**\n\nPlease try again later.\n\nSupport Group: @CantarellaBots",
+            "🚧 **Bot is under Maintenance!**\n\nPlease try again later.\n\nSupport Group: @log_channel_a",
             quote=True
         )
         return
