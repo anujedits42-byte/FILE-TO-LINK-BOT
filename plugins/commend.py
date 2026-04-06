1ad4d2b821e56dc689e0943703d21ccb2ba52442
anujedits42-byte <anujedits42@gmail.com>
2026-04-06 15:56:22 +0530

    Update commend.py

--- plugins/commend.py
+++ plugins/commend.py
@@ -18,7 +18,7 @@ import logging
 logger = logging.getLogger(__name__)
 BATCH_FILES = {}  
 
-   @Client.on_message(filters.command("start") & filters.incoming)
+@Client.on_message(filters.command("start") & filters.incoming)
 async def start(client, message):
     user_id = message.from_user.id
     mention = message.from_user.mention
