65591f596c43669c87a40f22e07f240a24829e3c
anujedits42-byte <anujedits42@gmail.com>
2026-04-13 18:58:18 +0530

    Update info.py

--- info.py
+++ info.py
@@ -20,27 +20,27 @@ SESSION = environ.get('SESSION', 'filetolinkbot')
 
 API_ID = get_int(environ.get('API_ID', '34724970'))
 API_HASH = environ.get('API_HASH', 'f240eae7c60e8e30c17203ab0e052f7e')
-BOT_TOKEN = environ.get('BOT_TOKEN', '8673437580:AAG1f0gCUbQ0lUD5BpozjhXg2OPLA8YSZFo')
+BOT_TOKEN = environ.get('BOT_TOKEN', '8266205685:AAGbjQwlglgz8TVDs-R1O51xRT3rEyzOEGg')
 
 # 👑 __Owner & Admins__
 ADMINS = get_list(environ.get('ADMINS', '7892805795'))
-AUTH_CHANNEL = get_list(environ.get("AUTH_CHANNEL", "-1003515041061"))
+AUTH_CHANNEL = get_list(environ.get("AUTH_CHANNEL", "-1003475522251"))
 
 OWNER_USERNAME = environ.get("OWNER_USERNAME", 'anujedits76')
 BOT_USERNAME = environ.get("BOT_USERNAME", 'file_to_link_ak_bot')
 
 # 🔗 __Channel & Support Links__
-CHANNEL = environ.get('CHANNEL', 'https://t.me/log_channel_a')
-SUPPORT = environ.get('SUPPORT', 'https://t.me/log_channel_a')
-HOW_TO_VERIFY = environ.get('HOW_TO_VERIFY', 'https://t.me/log_channel_a/238')
-HOW_TO_OPEN = environ.get('HOW_TO_OPEN', 'https://t.me/log_channel_a/238')
+CHANNEL = environ.get('CHANNEL', 'https://t.me/anujeditbyak')
+SUPPORT = environ.get('SUPPORT', 'https://t.me/anujeditbyak')
+HOW_TO_VERIFY = environ.get('HOW_TO_VERIFY', 'https://t.me/anujeditbyak')
+HOW_TO_OPEN = environ.get('HOW_TO_OPEN', 'https://t.me/anujeditbyak')
 
 # 📢 __Log Channels__
-BIN_CHANNEL = get_int(environ.get("BIN_CHANNEL", '-1003515041061'))
-LOG_CHANNEL = get_int(environ.get("LOG_CHANNEL", '-1003515041061'))
-PREMIUM_LOGS = get_int(environ.get("PREMIUM_LOGS", '-1003515041061'))
-VERIFIED_LOG = get_int(environ.get('VERIFIED_LOG', '-1003515041061'))
-SUPPORT_GROUP = get_int(environ.get("SUPPORT_GROUP", "-1003791508617"))
+BIN_CHANNEL = get_int(environ.get("BIN_CHANNEL", '-1003475522251'))
+LOG_CHANNEL = get_int(environ.get("LOG_CHANNEL", '-1003475522251'))
+PREMIUM_LOGS = get_int(environ.get("PREMIUM_LOGS", '-1003475522251'))
+VERIFIED_LOG = get_int(environ.get('VERIFIED_LOG', '-1003475522251'))
+SUPPORT_GROUP = get_int(environ.get("SUPPORT_GROUP", "-1003475522251"))
 
 # ✅ __Feature Toggles__
 VERIFY = False
@@ -92,7 +92,7 @@ HAS_SSL = get_bool(environ.get("HAS_SSL", "true"))
 
 # URL Generation
 import os as _os
-_default_domain = _os.environ.get("REPLIT_DEV_DOMAIN", "file-to-link-bot-9.onrender.com")
+_default_domain = _os.environ.get("REPLIT_DEV_DOMAIN", "file-to-link-bot-10.onrender.com")
 BIND_ADDRESS = environ.get("WEB_SERVER_BIND_ADDRESS", _default_domain)
 FQDN = environ.get("FQDN", BIND_ADDRESS)
 
